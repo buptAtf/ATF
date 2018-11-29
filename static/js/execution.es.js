c21a748f8990ffd8504ddf8162f6bbe73d89f5bf
lrq <aa651577562@qq.com>
2018-11-29 21:47:36 +0800

    在批量执行查询页面增加了查询

--- static/js/execution.es.js
+++ static/js/execution.es.js
@@ -326,4 +326,15 @@ function queryScene() {
     });
 }
 
+$('.form_date').datetimepicker({
+    language:  'fr',
+    weekStart: 1,
+    todayBtn:  1,
+    autoclose: 1,
+    todayHighlight: 1,
+    startView: 2,
+    minView: 2,
+    forceParse: 0
+});
+
 
