a2338aef084938aa1efc62a28b660d2a3adfc5f9
lrq <aa651577562@qq.com>
2019-04-18 21:20:32 +0800

    添加mock界面，基本完成预览页面的设计

--- static/js/datatable_flowcase.js
+++ static/js/datatable_flowcase.js
@@ -5,7 +5,13 @@ var app = new Vue({
         flowcaselist: ['流程1','流程2','流程3'],
     },
     ready: function(){
+
         this.getFlowcaseList();
+
+        $('.3').addClass('open');
+        $('.3 .arrow').addClass('open');
+        $('.3-ul').css({display: 'block'});
+        $('.3-3').css({color: '#ff6c60'});
     },
     methods:{
         getFlowcaseList: function(){
