30880d8386fef04062e1bc896a79e19769c42cef
lrq <aa651577562@qq.com>
2019-04-20 20:11:33 +0800

    1.数据中心流程用例的展示用例完成 2.mockserver界面添加后端接口初步完成

--- static/js/datatable_flowcase.js
+++ static/js/datatable_flowcase.js
@@ -2,11 +2,14 @@
 var app = new Vue({
     el: "#v-body",
     data: {
-        flowcaselist: ['流程1','流程2','流程3'],
+        flowcaselists: ['流程1','流程2','流程3'],
+        caseLibId: sessionStorage.getItem("caselibId"),
+        executorId: sessionStorage.getItem("userId"),
+
     },
     ready: function(){
 
-        this.getFlowcaseList();
+        this.getAllFlowcaseList();
 
         $('.3').addClass('open');
         $('.3 .arrow').addClass('open');
@@ -14,7 +17,22 @@ var app = new Vue({
         $('.3-3').css({color: '#ff6c60'});
     },
     methods:{
-        getFlowcaseList: function(){
+        getAllFlowcaseList: function(){
+            var _this = this;
+            $.ajax({
+                url: address3 + "dataCenter/queryFlowTestcase",
+                type: "post",
+                contentType: "application/json",
+                data: JSON.stringify({
+                    "executorId": _this.executorId,
+                    "caseLibId": _this.caseLibId,
+                }),
+                success: function(data){
+                    _this.flowcaselists = data.flowTestcaseInfos;
+                    console.log(data);
+                }
+
+            })
             
         }
 
--- static/js/mockserver.js
+++ static/js/mockserver.js
@@ -1,22 +1,72 @@
 var app = new Vue({
     el: "#v-mockserver",
     data: {
-        mockexce: [],
-        interface:"www.baidu.com",
+        
         creater: "刘福林",
-        path: "/dizhi",
         updateTime: "2019-4-18 20:49:00",
-        mockpath: "www.baidu.com"
-
+        allExpecation: [],
+        curExpecation: {},
+        curExpecationRet: {},
     },
     ready: function(){
 
+        this.getAllExpectation();
         $('.2').addClass('open');
         $('.2 .arrow').addClass('open');
         $('.2-ul').css({display: 'block'});
         $('.2-2').css({color: '#ff6c60'});
     },
     methods:{
+        getAllExpectation: function(){
+            var _this = this;
+            $.ajax({
+                url: address3 + "mockServer/getExpectation",
+                type: "post",
+                contentType: "application/json",
+                data: JSON.stringify({
+
+                }),
+                success:function(data){
+                    _this.curExpecation = data[0];
+                    data.forEach(element => {
+                        let tempItem = {};    
+                        tempItem.id = element.httpRequest.id;
+                        // tempitem.body = element.httpRequest.body;
+                        _this.allExpecation.push(tempItem);
+                        
+                    });
+                    
+                    _this.curExpecationRet = _this.curExpecation.httpResponse;                    
+                    _this.curExpecationRet = JSON.stringify(_this.curExpecationRet, null, 2);
+                    console.log(_this.curExpecationRet);
+                    // _this.curExpecationRet = syntaxHighlight(_this.curExpecationRet);
+                    // console.log(_this.curExpecationRet);
+                }
+
+            })
+        }
 
     }
 })
+
+function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+        json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function(match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
