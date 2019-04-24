34e6deff43f5ef084703ec110d85235d7adb2a48
lrq <aa651577562@qq.com>
2019-04-24 21:03:55 +0800

    在mock API界面新增添加期望的功能

--- static/js/mockserver.js
+++ static/js/mockserver.js
@@ -1,8 +1,6 @@
 var app = new Vue({
     el: "#v-mockserver",
     data: {
-        creater: "刘福林",
-        updateTime: "2019-4-18 20:49:00",
         allExpecation: [],
         curExpecation: {},
         curExpecationRet: {},
@@ -10,13 +8,28 @@ var app = new Vue({
     ready: function(){
 
         this.getAllExpectation();
-        $('.2').addClass('open');
+        $('.2').addClass('open');       //侧边展开，高亮显示当前页面Mock API
         $('.2 .arrow').addClass('open');
         $('.2-ul').css({display: 'block'});
         $('.2-2').css({color: '#ff6c60'});
     },
     methods:{
-        getAllExpectation: function(){
+        formatDate(date){       //格式化时间戳为标准日期
+            if(date){
+                var date = new Date(date);
+                var Y = date.getFullYear() + '-';
+                var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+                var D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + '/';
+                var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+                var m = (date.getMinutes() <10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+                var s = (date.getSeconds() <10 ? '0' + date.getSeconds() : date.getSeconds());
+                // console.log("时间是："+ Y+M+D+h+m+s);
+                return Y+M+D+h+m+s;  
+            }else{
+                return '';
+            }     
+        },
+        getAllExpectation: function(){      //得到所有的期望列表
             var _this = this;
             $.ajax({
                 url: address3 + "mockServer/getExpectation",
@@ -26,27 +39,40 @@ var app = new Vue({
 
                 }),
                 success:function(data){
-                    _this.curExpecation = data[0];
-                    data.forEach(element => {
-                        let tempItem = {};
-                        tempItem.id = element.httpRequest.id;
+                    _this.curExpecation = data[0];      //当前期望设置为第一条
+                    _this.allExpecation = data;
+                    data.forEach(element => {           //遍历所有数据，将所有对象放进期望列表数组中
+                        // let tempItem = {};
+                        // tempItem.id = element.httpRequest.id;
                         // tempitem.body = element.httpRequest.body;
-                        _this.allExpecation.push(tempItem);
+                        // _this.allExpecationRequest.push(tempItem);
                         
                     });
 
-                    _this.curExpecationRet = _this.curExpecation.httpResponse;
-                    _this.curExpecationRet = JSON.stringify(_this.curExpecationRet, null, 2);
+                    _this.curExpecationRet = _this.curExpecation.httpResponse;//当前期望的期望返回的数据
+                    _this.curExpecationRet = JSON.stringify(_this.curExpecationRet, null, 2);   //将返回的数据解析为JSON数据
                     console.log(_this.curExpecationRet);
-                    // _this.curExpecationRet = syntaxHighlight(_this.curExpecationRet);
                 }
 
             })
         },
-        queryExpecation: function(curId){
-            
-            console.log(curId);
-        }
+        queryExpecation: function(currentId){   //根据id查询当前期望
+            var _this = this;
+            $.ajax({
+                url: address3 + "mockServer/getExpectation",
+                type: "post",
+                contentType: "application/json",
+                data: JSON.stringify({
+                    "id": currentId
+                }),
+                success:function(data){
+                    console.log(data);
+                    _this.curExpecation = data;
+                }
+            }) 
+            console.log(currentId);
+        },
+
 
     }
 })
