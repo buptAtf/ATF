43d08ebdd6db116ca06609b74f00e7fc93fcdbd9
lrq <aa651577562@qq.com>
2019-04-30 19:15:54 +0800

    1.数据中心流程用例与后端部分联调 2.增加组件

--- static/js/datatable_flowcase.js
+++ static/js/datatable_flowcase.js
@@ -1,3 +1,59 @@
+var flowcaseTemplate =
+ `<section>
+ <h4 style="margin-left:30px; margin-top:150px"><span class="label label-info" >节点</span></h4>
+ <div class="col-lg-6 nopadding">
+   <h5><span class="col-lg-3 text-center">被测系统</span></h5>
+   <div class="col-lg-8">
+     <select type="text" class="form-control">
+         <option value="" selected>{{ nodeinfo.autName }}</option>
+         <option v-for="item in autList" value="item.id" >{{ item.nameMedium }}</option>
+     </select>
+   </div>
+ </div>
+
+ <div class="col-lg-6 nopadding">
+   <h5><span class="col-lg-3 text-center">功能点</span></h5>
+   <div class="col-lg-8">
+     <select type="text" class="form-control ">
+        <option value="" selected>{{ nodeinfo.transName }}</option>
+        
+     </select>
+   </div>
+ </div>
+
+ <div class="col-lg-6 nopadding">
+   <h5><span class="col-lg-3 text-center">动作标识</span></h5>
+   <div class="col-lg-8"> 
+       <input type="text" class="form-control" readonly="{{editFlag}}" v-model="nodeinfo.actioncode">
+   </div>
+ </div>
+
+ <div class="col-lg-6 nopadding">
+   <h5><span class="col-lg-3 text-center">步骤序号</span></h5>
+   <div class="col-lg-8">
+       <input type="text" class="form-control" readonly="{{editFlag}}" v-model="nodeinfo.steporder" >
+   </div>
+ </div>
+
+ <div class="col-lg-6 nopadding">
+   <h5><span class="col-lg-3 text-center">关联脚本</span></h5>
+   <h4><span class="label label-primary col-lg-3">查看脚本</span></h4>
+ </div>
+
+</section>`;
+
+// Vue.component('flowcase', {
+//     data: function() {
+//         return {
+//             count: 9
+//         }
+//     },
+//     props: ['nodeinfo'],
+//     template: flowcaseTemplate,
+
+// })
+
+
 
 var app = new Vue({
     el: "#v-body",
@@ -7,10 +63,37 @@ var app = new Vue({
         executorId: sessionStorage.getItem("userId"),
         currentCase: {},
         selectedCaseId: "",
+        autList: {},
+        editFlag: true,
+        nodeInfoList: {},
+
     },
     ready: function(){
+        var _this = this;
+        
+        _this.getAllFlowcaseList();
+        _this.queryAllAut();
 
-        this.getAllFlowcaseList();
+        // var p1 = new Promise((resolve, reject) => {
+        //     _this.getAllFlowcaseList();
+        //     console.log("id是:" + _this.selectedCaseId);
+        //     resolve();
+        // });
+        // p1.then(()=>{
+            
+        //     _this.queryThisCase(_this.selectedCaseId);
+        //     console.log("p1完成");
+        // })
+
+
+        // var p2 = new Promise((resolve,reject) => {
+        //     _this.queryThisCase(_this.selectedCaseId);
+        //     resolve();
+        // });
+        // var p = Promise.all([p1,p2]);
+        // p.then(()=>{
+        //     console.log("同步成功");
+        // })
 
         $('.3').addClass('open');
         $('.3 .arrow').addClass('open');
@@ -31,10 +114,9 @@ var app = new Vue({
                 success: function(data){
                     _this.flowcaselists = data.flowTestcaseInfos;
                     _this.selectedCaseId = _this.flowcaselists[0].id;
-                    console.log(data);
-                    
+                    console.log(data);   
+                    _this.queryThisCase(_this.selectedCaseId);
                 }
-
             })
             
         },
@@ -51,12 +133,53 @@ var app = new Vue({
                 success: function(data){
                     if(data.respCode=="0000"){
                         _this.currentCase = data;
-                        console.log(data);
+                        _this.nodeInfoList = data.nodeInfoList;
+                        console.log(_this.nodeInfoList);
                     }
                 }
             });
-        }
+        },
+        queryAllAut: function(){
+            var _this = this;
+            $.ajax({
+                url: address3 + "aut/queryListAut",
+                type: "post",
+                contentType: "application/json",
+                success: function(data){
+                    _this.autList = data.autRespDTOList;
+                    
+                }
+            })
+        },
+        queryTransactsByAutId: function(){
+            var _this = this;
+            $.ajax({
+                url: address3 + "aut/queryListAut",
+                type: "post",
+                async: false,
+                contentType: "application/json",
+                success: function(data){
+                    _this.autList = data.autRespDTOList;
+                    
+                }
+            })
+        },
 
+
+    },
+    components:{
+        "flowcase":{
+            template: flowcaseTemplate,
+            props: ["nodeinfo", "aut", "editFlag"],
+
+        }
     }
 
 })
+
+var flowcaseFun = function(){
+
+};
+
+
+
