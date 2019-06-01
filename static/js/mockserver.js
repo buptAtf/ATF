a0d315aa6aad8e5643d9adc33bf140b18421193f
lrq <aa651577562@qq.com>
2019-06-01 17:11:35 +0800

    mockapi页面编辑功能完成，运行功能完成一半与阿甘联调

--- static/js/mockserver.js
+++ static/js/mockserver.js
@@ -5,19 +5,24 @@ var app = new Vue({
         curExpecation: {httpRequest:""},      //当前的期望
         curExpecationRet: {},   //当前期望的返回
         editCurData: {id:"",expectationName:"",creator:"",httpRequest:"",httpResponse:"",httpForwardEntity:"",type:""},
+        runData:{method:"", path:"", body:"", type:"", headers:"", cookies:"", keepAlive:"", secure:"", pathType:true, methodType: true},
         selectedExpId: "",      //选中的期望的id
         requestParams: [{key:"",value:""}],       //规则的请求的参数
         responseParams: [{key:"",value:""}],    //规则的返回参数
+        runParams: [{key:"",value:""}],     //运行的参数
         reqHeaders: [{key:"",value:""}],    //请求头部的值
-        respHeaders: [{key:"",value:""}], //返回头部的值
+        respHeaders: [{key:"",value:""}],   //返回头部的值
+        runHeaders: [{key:"",value:""}],    //运行的头部
         reqCookies: [{key:"",value:""}],    //请求cookies
-        respCookies: [{key:"",value:""}], //返回cookise的值
-        keepalive: "",      //请求报文中的长连接 
+        respCookies: [{key:"",value:""}],   //返回cookise的值
+        runCookies: [{key:"",value:""}],    //运行的cookies
+        keepalive: "",               //请求报文中的长连接 
         newCreator: "",            //创建人
         newExpectationName: "",    //期望名称
         newAction: "response",             //动作
         editReqFlag: "1",              //请求参数设置显示的标志位
         editRespFlag: "1",
+        runFlag: "1",
         respforwardFlag: "response",    //返回的数据是response还是forward
     },
     ready: function(){
@@ -91,12 +96,28 @@ var app = new Vue({
                         _this.curExpecationRet = data.httpForwardEntity; //当前期望的期望返回的数据
                     }
                     _this.curExpecationRet = JSON.stringify(_this.curExpecationRet, null, 2);   //将返回的数据解析为JSON数据
-                    
                     _this.processDisplayData();    //处理返回的数据，用于显示编辑页面中的headers等类型的数据
                     
+                    if(data.runRequestId!==null){
+                        _this.queryRunInfo(data.runRequestId);
+                    }
                 }
             }) 
             
+        },
+        queryRunInfo: function(id) {      //查询上次用户退出时，运行的信息
+            $.ajax({
+                url: address3 + "/mockServer/getRunRequest",
+                type: "post",
+                contentType: "application/json",
+                data: {
+                    "id": id
+                },
+                success: function(data) {
+
+                    console.log(data);
+                }
+            })
         },
         addRequestList: function(){
             this.requestParams.push({key:"",value:""});
@@ -112,6 +133,30 @@ var app = new Vue({
         delResponseList: function(index){
             if(this.responseParams.length !== 1){
                 this.responseParams.splice(index,1);
+            }
+        },
+        addRunParams: function() {      //增加运行参数
+            this.runParams.push({key:"",value:""});
+        },
+        delRunParams: function(index) { //删除运行参数
+            if(this.runParams.length !== 1){
+                this.runParams.splice(index,1);
+            }
+        },
+        addRunHeaders: function() {
+            this.runHeaders.push({key:"",value:""});
+        },
+        delRunHeaders: function(index) {
+            if(this.runHeaders.length !== 1){
+                this.runHeaders.splice(index,1);
+            }
+        },
+        addRunCookies: function() {
+            this.runCookies.push({key:"",value:""});
+        },
+        delRunCookies: function() {
+            if(this.runCookies.length !== 1){
+                this.runCookies.splice(index,1);
             }
         },
         addReqHeaders: function(){
@@ -202,9 +247,13 @@ var app = new Vue({
                 return JSON.stringify(ret);     //返回的数据是一个字符串
             }
 
-            _this.editCurData.httpRequest.headers = processPostData(_this.reqHeaders);      //处理请求的headers
-            _this.editCurData.httpRequest.cookies = processPostData(_this.reqCookies);      //处理请求的cookies
-            _this.editCurData.httpRequest.queryParameters = processPostData(_this.requestParams);   //处理请求的参数
+            if(_this.editCurData.httpRequest!==null){
+                _this.editCurData.httpRequest.headers = processPostData(_this.reqHeaders);      //处理请求的headers
+                _this.editCurData.httpRequest.cookies = processPostData(_this.reqCookies);      //处理请求的cookies
+                _this.editCurData.httpRequest.queryParameters = processPostData(_this.requestParams);   //处理请求的参数
+            }
+
+            
             if(_this.editCurData.httpResponse!==null){      //如果返回不是null的话，再进行操作
                 _this.editCurData.httpResponse.headers = processPostData(_this.respHeaders);
                 _this.editCurData.httpResponse.cookies = processPostData(_this.respCookies);
@@ -247,7 +296,7 @@ var app = new Vue({
                 }
             }
 
-            if(_this.editCurData.httpRequest.type==='JSON'){    //如果请求的数据body是json格式的话，在前端进行json格式展示
+            if(_this.editCurData.httpRequest!==null && _this.editCurData.httpRequest.type==='JSON'){    //如果请求的数据body是json格式的话，在前端进行json格式展示
                 let body = JSON.parse(_this.editCurData.httpRequest.body);
                 _this.editCurData.httpRequest.body = JSON.stringify(body, null, 2);   //将返回的数据解析为JSON数据
             }
@@ -255,7 +304,6 @@ var app = new Vue({
                 let body = JSON.parse(_this.editCurData.httpResponse.body);
                 _this.editCurData.httpResponse.body = JSON.stringify(body, null, 2);   //将返回的数据解析为JSON数据
             }
-            console.error("难受啊马飞")
             
             _this.requestParams = [{key:"",value:""}];   //先清除请求数据的参数，再重新赋值，不然会累积之前冗余项
             _this.reqHeaders = [{key:"",value:""}];   //先清除请求数据的headers，再重新赋值，不然会累积之前冗余项
@@ -263,24 +311,68 @@ var app = new Vue({
             _this.respHeaders = [{key:"",value:""}];
             _this.respCookies = [{key:"",value:""}];
 
-            processStep(_this.editCurData.httpRequest.queryParameters, _this.requestParams);        //处理用于发送请求的“请求的参数”和显示的请求的参数
-            processStep(_this.editCurData.httpRequest.headers, _this.reqHeaders);
-            processStep(_this.editCurData.httpRequest.cookies, _this.reqCookies);
+            if(_this.editCurData.httpRequest!==null){
+                processStep(_this.editCurData.httpRequest.queryParameters, _this.requestParams);        //处理用于发送请求的“请求的参数”和显示的请求的参数
+                processStep(_this.editCurData.httpRequest.headers, _this.reqHeaders);
+                processStep(_this.editCurData.httpRequest.cookies, _this.reqCookies);
+            }
             if(_this.editCurData.httpResponse!==null){  //当返回的数据不是null的时候，再进行数据操作
                 processStep(_this.editCurData.httpResponse.headers, _this.respHeaders);
                 processStep(_this.editCurData.httpResponse.cookies, _this.respCookies);
             }
+        },
+
+        runCurExpectation: function() {
+            const _this = this;
+
+            var processPostData = function(source){     //处理要发送的数据，把页面中用户输入好的数据转变为json字符串形式
+                let ret = {};
+                for(let i=0; source[i]!=undefined && source[i].key!=""; i++){
+                    ret[source[i].key] = source[i].value;
+                }
+                return JSON.stringify(ret);     //返回的数据是一个字符串
+            }
+            _this.runData.headers = processPostData(_this.runHeaders);      //处理请求的headers
+            _this.runData.cookies = processPostData(_this.runCookies);      //处理请求的cookies
+            _this.runData.queryParameters = processPostData(_this.runParams);   //处理请求的参数
             
+            $.ajax({
+                url: address3 + "/mockServer/runExpectation",
+                type: "post",
+                contentType: "application/json",
+                data: JSON.stringify(_this.runData),
+                success: function(data) {
+
+                    console.log(data)
+                    if(data.respCode==='0000'){
+                        //运行结束，保存当前数据
+                        $.ajax({
+                            url: address3+ "/mockServer/saveRunRequest",
+                            type: "post",
+                            contentType: "application/json",
+                            data: JSON.stringify(_this.runData),
+                            success: function(data) {
+                                console.log(data);
+                            },
+
+                        })
+                    }
+                    
+                }
+            })
         },
 
         changeReqEditFlag: function(flag){  //改变点击按钮的标志位，用于显示请求数据中不同的项目
-            var _this = this;
-            _this.editReqFlag = flag;
+            
+            this.editReqFlag = flag;
         },
         changeRespEditFlag: function(flag){ //改变点击按钮的标志位，用于显示返回数据中不同的项目
-            var _this = this;
-            _this.editRespFlag = flag;
+            
+            this.editRespFlag = flag;
         },
+        changeRunFlag: function(flag) {
+            this.runFlag = flag;
+        }
 
     }
 })
