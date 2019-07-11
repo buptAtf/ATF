d714497376ea3964f6f2d5819024484fa16228c1
lrq <aa651577562@qq.com>
2019-07-11 22:23:23 +0800

    输入框设计完成，elementId字段也已添加，下拉框等待佳轩后端给字段

--- static/js/ruleinput.js
+++ static/js/ruleinput.js
@@ -5,7 +5,15 @@
       return {
         ruleName: '',
         ruleDesc: '',
+        modal: {type:'',elementId:''},
         elements: {},
+        isRestraint: false, //输入框是否有限制
+        inputMust: false,   //输入框是否必输
+        inputValue: '',     //输入框的标准值
+        inputMinLength: '', //输入的最短长度
+        inputMaxLength: '', //输入的最大长度
+        inputElement:[],    //输入元素的字段
+        setOrder: 0,        //输入顺序
       }
     },
     ready: function() {
@@ -31,11 +39,9 @@
             Vac.alert("功能正在努力开发中.......loading");    
         },
        
-
-        getAllElements: function() {
+        getAllElements: function() {    //请求后端，当前的所有元素
             const _this = this;
-            let transactId = sessionStorage.getItem("transactId");
-            
+            let transactId = sessionStorage.getItem("transactId");  
             $.ajax({
                 url: address3 + "elementRepository/queryAllElementsForATransact",
                 type: "POST",
@@ -45,15 +51,49 @@
                 }),
                 success: function(data) {
                     _this.elements = data.uis[0].elements;
-                    console.log(_this.elements);
+                    
+                    
                 }
             })
-
         },
-        makeRules: function(kind) {
-            console.log(kind)
+        setRules: function() {      //设置规则
+            const _this = this;
+            this.setOrder++;    //记录用户的顺序
+            if(_this.modal.type=='webedit') {
+                var inputEle = {};
+                inputEle.inputMust = _this.inputMust;       //设置是否是必输项
+                inputEle.isRestraint = _this.isRestraint;       //是否设置规则
+                inputEle.inputValue = _this.inputValue;         //标准输入值
+                inputEle.inputMinLength = _this.inputMinLength; //输入最小值
+                inputEle.inputMaxLength = _this.inputMaxLength; //输入最大值
+                inputEle.inputSpecialCh = getCheckBoxValue();   //复选框的值，不能包含的字符
+                inputEle.order = _this.setOrder;
+                inputEle.elementId = _this.modal.elementId;
+                _this.inputElement.push(inputEle);    //将设置好的输入框元素推入数组
 
-        }
+                console.log(JSON.stringify(inputEle));
+            } else if(_this.modal.type=='weblist') {
+                
+
+
+            }
+        },
+        setCurType: function(type , elementId) {    //确定当前点击的模态框的类型
+            this.modal.type = type;
+            this.modal.elementId = elementId;
+        },
+
     },
 
   })
+
+
+
+function getCheckBoxValue() {
+//获取input类型是checkBox并且 name="box"选中的checkBox的元素
+    var data = $('input:checkbox[name="inputSpecialCh"]:checked').map(function () {
+    return $(this).val();
+    }).get().join(",");
+
+    return data;
+}
