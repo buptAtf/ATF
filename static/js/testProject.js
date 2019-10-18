920b1ecc23b1222e16b64263fdf8b3d8a63500b7
ClearLiu <13051324554@163.com>
2019-10-18 22:55:28 +0800

    上传

--- static/js/testProject.js
+++ static/js/testProject.js
@@ -108,7 +108,7 @@ var app = new Vue({
                     type: 'post',
                     contentType: 'application/json',
                     data: JSON.stringify({
-                        codeLongAndName: testProjectCode,
+                        codeLong: testProjectCode,
                         nameMedium: testProjectName,
                         descMedium:  taskDescription 
                     }),
@@ -186,6 +186,7 @@ var app = new Vue({
             var _this=this
             var selectedInput = $('input[name="chk_list"]:checked');
             var id = selectedInput.attr('id');
+            console.log(id)
             var realId=id.substring(0,3);
             $.ajax({
                 url: address3 + 'testProjectController/modifySingleTestProject',
@@ -193,7 +194,7 @@ var app = new Vue({
                 contentType: 'application/json',
                 data: JSON.stringify({
                         id: realId,
-                        codeLongAndName: $('#updateForm input[name="codeLong"]').val(),
+                        codeLong: $('#updateForm input[name="codeLong"]').val(),
                         nameMedium: $('#updateForm input[name="nameMedium"]').val(),
                         descMedium: $('#updateForm textarea[name="descMedium"]').val()
                     }),
